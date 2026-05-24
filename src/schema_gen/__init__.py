7870031e9bd7bd2aa576df8d1ea8af70be02fc2f
NRIPANKA <NRIPANKADAS@GMAIL.COM>
2026-05-24 15:11:15 +0400

    Harden portfolio quality baseline

--- src/schema_gen/__init__.py
+++ src/schema_gen/__init__.py
@@ -2,4 +2,6 @@
 
 __version__ = "0.1.0"
 
-from schema_gen.generator import generate_model, generate_models_from_file, infer_schema
+from schema_gen.generator import generate_model as generate_model
+from schema_gen.generator import generate_models_from_file as generate_models_from_file
+from schema_gen.generator import infer_schema as infer_schema
